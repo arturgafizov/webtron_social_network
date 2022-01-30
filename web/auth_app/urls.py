6ac56dcb84a8eb272e100fae3284ae450b5d2db1
Artur <art05xxx@rambler.ru>
2022-01-30 14:30:38 +0300

    22 commit rename url

--- web/auth_app/urls.py
+++ web/auth_app/urls.py
@@ -7,6 +7,6 @@ app_name = 'auth_app'
 
 urlpatterns = [
     path('sign-in/', LoginView.as_view(), name='api_login'),
-    path('clients/create/', SignUpView.as_view(), name='api_sign_up'),
+    path('sign_up/', SignUpView.as_view(), name='api_sign_up'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
