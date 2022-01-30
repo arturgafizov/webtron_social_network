0ec975836aeca7b488f1bdd1037405f470c5b73b
Artur <art05xxx@rambler.ru>
2022-01-30 14:35:00 +0300

    27 commit create PostViewSet

--- web/posts/views.py
+++ web/posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework import status
 
-# Create your views here.
+from .models import Post
+from . import serializers
+
+
+class PostViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated, )
+
+    def get_serializer_class(self):
+        return serializers.PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def create(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+    def update(self, request, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
