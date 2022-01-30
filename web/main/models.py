6e461192bb094956a7b1bb16f5489a7672ef4a3e
Artur <art05xxx@rambler.ru>
2022-01-30 14:19:59 +0300

    13 commit update User model

--- web/main/models.py
+++ web/main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.utils.safestring import mark_safe
 
 
 from .managers import UserManager
@@ -21,7 +20,6 @@ class User(AbstractUser):
     email = models.EmailField(_('Email address'), unique=True)
     gender = models.CharField(max_length=6, choices=GenderChoice.choices)
     avatar = models.ImageField(upload_to=avatar_upload_patch, default='default_avatar.jpeg')
-    like = models.ManyToManyField('self', related_name='subscribers', symmetrical=False, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
