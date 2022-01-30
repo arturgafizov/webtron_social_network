56ff517e3da53c49d992204b124613d550b7b05f
Artur <art05xxx@rambler.ru>
2022-01-30 14:20:11 +0300

    13 commit create Post model

--- web/posts/models.py
+++ web/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from actions.choices import LikeStatus
+from .choices import PostStatus
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models. ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='post_set')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    status = models.PositiveSmallIntegerField(choices=PostStatus.choices, default=PostStatus.INACTIVE)
+
+    def likes(self) -> int:
+        return self.post_likes.filter(vote=LikeStatus.LIKE).count()
+
+    def dislikes(self) -> int:
+        return self.post_likes.filter(vote=LikeStatus.DISLIKE).count()
