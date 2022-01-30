f2c680344414e4664508c50dc3d9f036ee3ea8a6
Artur <art05xxx@rambler.ru>
2022-01-30 14:21:51 +0300

    16 commit update UserSerializer

--- web/main/serializers.py
+++ web/main/serializers.py
@@ -1,12 +1,14 @@
+from datetime import datetime
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'gender', 'avatar', 'email')
+        fields = ('first_name', 'last_name', 'gender', 'email')
         extra_kwargs = {
             'avatar': {'read_only': False},
         }
